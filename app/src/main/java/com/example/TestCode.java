f82af884a064920f740233ef61e85c908f45d2be
Nitin Khurana <nitin070895a>
2021-09-24 15:51:15 +0530

    Updated.

--- app/src/main/java/com/example/TestCode.java
+++ app/src/main/java/com/example/TestCode.java
@@ -5,7 +5,7 @@ public class TestCode {
     public void testing() {
         int a = 1;
         int b = 2;
-        System.out.println("a + b = " + a + b);
+        System.out.println("a + b = " + (a + b));
     }
 }
 
