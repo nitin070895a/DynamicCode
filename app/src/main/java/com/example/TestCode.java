ba1acbf46c8d234d032e4a769b5a9e6df7c63662
Nitin Khurana <nitin070895a>
2021-09-24 15:06:39 +0530

    Removed files

--- app/src/main/java/com/example/TestCode.java
+++ app/src/main/java/com/example/TestCode.java
@@ -2,13 +2,11 @@ package com.example;
 
 public class TestCode {
 
-    public static void main(String[] args) {
-
-    }
-
-    void testing() {
+    public void testing() {
         int a = 1;
         int b = 2;
         System.out.println("a + b = " + a + b);
     }
 }
+
+
